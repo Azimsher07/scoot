edb08a8bb5d5c6c1167992fb99af7b59eea6f278
Azimsher07 <Azimsher070@gmail.com>
2023-12-12 13:18:52 +0500

    our missions | our things finish

--- src/components/pages/CareersComponent/ourMission/OurMission.jsx
+++ src/components/pages/CareersComponent/ourMission/OurMission.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import manImg from "/images/otherPages/CareersImages/man.png";
+import { Button, Heading2, Paragraph } from "../../../common";
 
 export const OurMission = () => {
-  return <div>OurMission</div>;
+  return (
+    <div>
+      <div className="py-[120px] dfCol dfCenter text-center gap-[32px]">
+        <img src={manImg} className="rounded-full mb-[30px]" alt="man" />
+        <Heading2>Care to join our mission?</Heading2>
+        <Paragraph>
+          We’re always looking for ambitious individuals to help us on our
+          journey. If you’re passionate about our mission to provide clean,
+          accessible transport to improve urban living we want to hear from you!
+        </Paragraph>
+        <Button>Say Hello</Button>
+      </div>
+      
+    </div>
+  );
 };
--- src/pages/Careers/Careers.jsx
+++ src/pages/Careers/Careers.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { OurMission } from "../../components/pages/CareersComponent";
+import { OurThings } from "../../components/common";
 
 export const Careers = () => {
   return (
     <section>
       <div className="smPadding">
         <OurMission />
+        <OurThings>Why join us?</OurThings>
+        
       </div>
     </section>
   );
